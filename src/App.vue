9fec86bd847fb62c3c74a916a3cd37a16e6ff879
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2021-12-29 21:05:13 +0100

    Lesson 24: Component Styles & Global Styles

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import Modal from "./components/Modal.vue";
+import Modal from "./components/Modal";
 
 export default {
   name: "App",
@@ -13,16 +13,7 @@ export default {
       title: "My First Vue App",
     };
   },
-  components: {
-    Modal,
-  },
-  methods: {
-    handleClick() {
-      console.log(this.$refs.name);
-      this.$refs.name.classList.add("active");
-      this.$refs.focus();
-    },
-  },
+  components: { Modal },
 };
 </script>
 
@@ -34,5 +25,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
